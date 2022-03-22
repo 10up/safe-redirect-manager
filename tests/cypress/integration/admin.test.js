1433d60eadf89f60b4f6a812e965e77a746c8931
Dharmesh Patel <dspatel44@gmail.com>
2022-03-22 12:48:56 +0530

    Added admin related tests.

--- tests/cypress/integration/admin.test.js
+++ tests/cypress/integration/admin.test.js
@@ -1,12 +1,31 @@
 describe( 'Admin can login and make sure plugin is activated', () => {
-	it( 'Can activate plugin if it is deactivated', () => {
+
+	beforeEach(() => {
 		cy.login();
+	});
+
+	it( 'Can activate plugin if it is deactivated', () => {
     	cy.visit('/wp-admin/plugins.php');
 
 		cy.get( '#deactivate-safe-redirect-manager' ).click();
 		cy.get( '#activate-safe-redirect-manager' ).click();
 		cy.get( '#deactivate-safe-redirect-manager' ).should( 'be.visible' );
-	} );
-} );
+	});
+
+	it( 'Can see "Safe Redirect Manager" menu item under Tools menu', () => {
+		cy.visit('/wp-admin/');
+
+		// Check menu item under Tools menu.
+		cy.get('#menu-tools ul.wp-submenu li').filter(':contains("Safe Redirect Manager")').should('have.length', 1)
+	});
+
+	it('Can visit "Safe Redirect Manager" page', () => {
+		cy.visit('/wp-admin/edit.php?post_type=redirect_rule');
+
+		// Check Heading and create link.
+		cy.get('#wpbody h1.wp-heading-inline').contains('Safe Redirect Manager');
+		cy.get('#wpbody a.page-title-action').should('have.text', 'Create Redirect Rule');
+	});
+});
 
 
