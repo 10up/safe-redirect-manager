9c4a7e271cbc4fc387e0bafed9690318802f7ce5
Dharmesh Patel <dspatel44@gmail.com>
2022-03-22 15:09:25 +0530

    Removed WPAcceptance Tests

--- tests/cypress/integration/admin.test.js
+++ tests/cypress/integration/admin.test.js
@@ -1,31 +1,33 @@
-describe( 'Admin can login and make sure plugin is activated', () => {
-
+describe("Admin can login and make sure plugin is activated", () => {
 	beforeEach(() => {
 		cy.login();
 	});
 
-	it( 'Can activate plugin if it is deactivated', () => {
-    	cy.visit('/wp-admin/plugins.php');
+	it("Can activate plugin if it is deactivated", () => {
+		cy.visit("/wp-admin/plugins.php");
 
-		cy.get( '#deactivate-safe-redirect-manager' ).click();
-		cy.get( '#activate-safe-redirect-manager' ).click();
-		cy.get( '#deactivate-safe-redirect-manager' ).should( 'be.visible' );
+		cy.get("#deactivate-safe-redirect-manager").click();
+		cy.get("#activate-safe-redirect-manager").click();
+		cy.get("#deactivate-safe-redirect-manager").should("be.visible");
 	});
 
-	it( 'Can see "Safe Redirect Manager" menu item under Tools menu', () => {
-		cy.visit('/wp-admin/');
+	it('Can see "Safe Redirect Manager" menu item under Tools menu', () => {
+		cy.visit("/wp-admin/");
 
 		// Check menu item under Tools menu.
-		cy.get('#menu-tools ul.wp-submenu li').filter(':contains("Safe Redirect Manager")').should('have.length', 1)
+		cy.get("#menu-tools ul.wp-submenu li")
+			.filter(':contains("Safe Redirect Manager")')
+			.should("have.length", 1);
 	});
 
 	it('Can visit "Safe Redirect Manager" page', () => {
-		cy.visit('/wp-admin/edit.php?post_type=redirect_rule');
+		cy.visit("/wp-admin/edit.php?post_type=redirect_rule");
 
 		// Check Heading and create link.
-		cy.get('#wpbody h1').contains('Safe Redirect Manager');
-		cy.get('#wpbody a.page-title-action').should('have.text', 'Create Redirect Rule');
+		cy.get("#wpbody h1").contains("Safe Redirect Manager");
+		cy.get("#wpbody a.page-title-action").should(
+			"have.text",
+			"Create Redirect Rule"
+		);
 	});
 });
-
-
--- tests/cypress/integration/safe-redirect-manager.test.js
+++ tests/cypress/integration/safe-redirect-manager.test.js
@@ -1,43 +1,54 @@
-describe( 'Test redirect rules', () => {
-
+describe("Test redirect rules", () => {
 	beforeEach(() => {
 		cy.login();
 	});
 
 	after(() => {
 		cy.deleteRedirectRules();
-	})
+	});
 
-	it( 'Can create a rule', () => {
+	it("Can create a rule", () => {
 		// Test Create Rule.
-		cy.createRedirectRule('/test', '/test2', 'sample rule note');
+		cy.createRedirectRule("/test", "/test2", "sample rule note");
 
 		// Validate created rule in list table.
-		cy.visit('/wp-admin/edit.php?post_type=redirect_rule');
-		cy.get('#the-list td.title a').first().should('have.text', '/test');
-		cy.get('#the-list td.srm_redirect_rule_to').first().should('have.text', '/test2');
-		cy.get('#the-list td.srm_redirect_rule_status_code').first().should('have.text', '302');
+		cy.visit("/wp-admin/edit.php?post_type=redirect_rule");
+		cy.get("#the-list td.title a").first().should("have.text", "/test");
+		cy.get("#the-list td.srm_redirect_rule_to")
+			.first()
+			.should("have.text", "/test2");
+		cy.get("#the-list td.srm_redirect_rule_status_code")
+			.first()
+			.should("have.text", "302");
 	});
 
-	it( 'Can redirect a simple rule request', () => {
-		cy.createRedirectRule('/first-blog', '/hello-world', 'Simple rule note');
+	it("Can redirect a simple rule request", () => {
+		cy.createRedirectRule(
+			"/first-blog",
+			"/hello-world",
+			"Simple rule note"
+		);
 
-		cy.visit('/first-blog');
-		cy.url().should('include', '/hello-world');
+		cy.visit("/first-blog");
+		cy.url().should("include", "/hello-world");
 	});
 
-	it( 'Can redirect a wildcard rule request', () => {
-		cy.createRedirectRule('/test*', '/sample-page', 'Wildcard rule note');
+	it("Can redirect a wildcard rule request", () => {
+		cy.createRedirectRule("/test*", "/sample-page", "Wildcard rule note");
 
-		cy.visit('/test-1');
-		cy.url().should('include', '/sample-page');
+		cy.visit("/test-1");
+		cy.url().should("include", "/sample-page");
 	});
 
+	it("Can redirect a Regex rule request", () => {
+		cy.createRedirectRule(
+			"/blog/(.*)",
+			"/hello-world",
+			"Regex rule note",
+			true
+		);
 
-	it( 'Can redirect a Regex rule request', () => {
-		cy.createRedirectRule('/blog/(.*)', '/hello-world', 'Regex rule note', true);
-
-		cy.visit('/blog/1');
-		cy.url().should('include', '/hello-world');
+		cy.visit("/blog/1");
+		cy.url().should("include", "/hello-world");
 	});
 });
